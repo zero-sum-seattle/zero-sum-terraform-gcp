57d8ec492275662639b1ed54e4991abc0e4fa7ba
MattsFace <spahmatthew@gmail.com>
2022-09-04 16:40:06 -0700

    Basic firewall configuration

--- test/outputs.tf
+++ test/outputs.tf
@@ -1,3 +1,7 @@
 output "state_bucket" {
   value = google_storage_bucket.tfstate.name
 }
+
+output "cluster_name" {
+  value = google_container_node_pool.primary_nodes.name 
+}
--- test/variables.tf
+++ test/variables.tf
@@ -1,4 +1,11 @@
 variable "gke_number_nodes" {
+  type        = number
   default     = 2
   description = "the number of nodes in a gke pool"
+}
+
+variable "network" {
+  type           = string
+  default        = "main-vpc"
+  description    = "the number of nodes in a gke pool"
 }
